Bug fixes on net calculation and adding ability to specify owned portion of royalty

resaid/econ.py
@@ -85,6 +85,7 @@ class well_econ:
 
         # optional variables
         self._royalty_col = None
+        self._owned_royalty_col = None
 
         self._wi_col = None
         self._nri_col = None
@@ -220,6 +221,14 @@ class well_econ:
     def royalty_col(self,value):
         self._royalty_col = value
         
+    @property
+    def owned_royalty_col(self):
+        return self._owned_royalty_col
+
+
+    @owned_royalty_col.setter
+    def owned_royalty_col(self,value):
+        self._owned_royalty_col = value
 
     @property
     def flowstreams(self):
@@ -530,29 +539,43 @@ class well_econ:
         l_flow['gas_price'] = self.generate_gas_price(t_series)
         l_flow['ngl_price'] = l_flow['oil_price'] * self._ngl_price_fraction
 
+        l_flow['oil_revenue'] = l_flow[self.OIL_COL]*l_flow['oil_price']
+        l_flow['gas_revenue'] = l_flow['gas_sold']*l_flow['gas_price']
+        l_flow['ngl_revenue'] = l_flow['ngl_volume']*l_flow['ngl_price']
+
         l_flow['revenue'] = (
             l_flow[self.OIL_COL]*l_flow['oil_price']
             +l_flow['gas_sold']*l_flow['gas_price']
             +l_flow['ngl_volume']*l_flow['ngl_price']
         )
 
-        if self._wi_col and self._nri_col:
+        
+        if self._wi_col and self._nri_col and self._owned_royalty_col and self._royalty_col:
             l_nri = self._header_data[self._header_data[self._header_uwi_col]==input_well].iloc[0][self._nri_col]
             l_wi = self._header_data[self._header_data[self._header_uwi_col]==input_well].iloc[0][self._wi_col]
+            l_ori = self._header_data[self._header_data[self._header_uwi_col]==input_well].iloc[0][self._owned_royalty_col]
+            l_royalty = self._header_data[self._header_data[self._header_uwi_col]==input_well].iloc[0][self._royalty_col]
+            if l_nri+l_royalty>1:
+                raise Exception(ValueError, f"Sum of royalty and NRI must be less than or equal to 1, currently {l_nri+l_royalty}")
+            l_flow['royalty'] = l_flow['revenue']*l_royalty
+            
+        elif self._wi_col and self._nri_col:  
+            l_nri = self._header_data[self._header_data[self._header_uwi_col]==input_well].iloc[0][self._nri_col]
+            l_wi = self._header_data[self._header_data[self._header_uwi_col]==input_well].iloc[0][self._wi_col]
+            l_ori = 0
             l_royalty = 1-l_nri/l_wi
             l_flow['royalty'] = l_flow['revenue']*l_royalty
         elif self._royalty_col:
             l_royalty = self._header_data[self._header_data[self._header_uwi_col]==input_well].iloc[0][self._royalty_col]
+            l_ori = 0
             l_flow['royalty'] = l_flow['revenue']*l_royalty
             l_wi = 1
+            l_nri = 1-l_royalty
         else:
             l_royalty = self._royalty
             l_flow['royalty'] = l_flow['revenue']*self._royalty
             l_wi = 1
-
-        l_flow['net_oil'] = l_flow[self.OIL_COL]*l_royalty
-        l_flow['net_gas'] = l_flow['gas_sold']*l_royalty
-        l_flow['net_ngl'] = l_flow['ngl_volume']*l_royalty
+            l_nri = 1-l_royalty
 
         l_flow['expense'] = (
             self._opc_t
@@ -566,6 +589,13 @@ class well_econ:
             0,
             l_flow['expense']
         )
+
+        l_flow['severance_tax'] = (
+            self._sev_gas*l_flow[self.GAS_COL]*l_flow['gas_price']
+            + self._sev_oil*l_flow[self.OIL_COL]*l_flow['oil_price']
+        )*(1-l_royalty)
+
+        l_flow['ad_val_tax'] = self._atx*l_flow['revenue']*(1-l_royalty)
 
         l_flow['taxes'] = (
             self._atx*l_flow['revenue']
@@ -603,12 +633,14 @@ class well_econ:
             'gas_sold',
             self.WATER_COL,
             'ngl_volume',
-            'net_oil',
-            'net_gas',
-            'net_ngl',
+            'oil_revenue',
+            'gas_revenue',
+            'ngl_revenue',
             'revenue',
             'royalty',
             'expense',
+            'severance_tax',
+            'ad_val_tax',
             'taxes',
             'capex',
             'cf',
@@ -617,27 +649,56 @@ class well_econ:
 
         l_flow = self.zero_below(l_flow,last_cf,zero_cols)
 
-        l_flow[['wi_net_oil',
-            'wi_net_gas',
-            'wi_net_ngl',
+        # calculate WI vales
+        l_flow[['wi_oil',
+            'wi_gas',
+            'wi_ngl',
             'wi_revenue',
             'wi_royalty',
             'wi_expense',
+            'wi_severance_tax',
+            'wi_ad_val_tax',
             'wi_taxes',
             'wi_capex',
             'wi_cf',
-            'wi_dcf']] = l_flow[['net_oil',
-            'net_gas',
-            'net_ngl',
+            'wi_dcf']] = l_flow[[self.OIL_COL,
+            'gas_sold',
+            'ngl_volume',
             'revenue',
             'royalty',
             'expense',
+            'severance_tax',
+            'ad_val_tax',
             'taxes',
             'capex',
             'cf',
             'dcf']].mul(l_wi)
+        
+        l_flow['net_oil'] = l_flow[self.OIL_COL]*(l_nri+l_royalty*l_ori)
+        l_flow['net_gas'] = l_flow['gas_sold']*(l_nri+l_royalty*l_ori)
+        l_flow['net_ngl'] = l_flow['ngl_volume']*(l_nri+l_royalty*l_ori)
+        l_flow['net_oil_revenue'] = l_flow['oil_revenue']*(l_nri+l_royalty*l_ori)
+        l_flow['net_gas_revenue'] = l_flow['gas_revenue']*(l_nri+l_royalty*l_ori)
+        l_flow['net_ngl_revenue'] = l_flow['ngl_revenue']*(l_nri+l_royalty*l_ori)
+        l_flow['net_revenue'] = l_flow['revenue']*(l_nri+l_royalty*l_ori)
+        l_flow['net_royalty'] = 0
+        l_flow['net_expense'] = l_flow['wi_expense']
+        l_flow['net_severance_tax'] = l_flow['severance_tax']/(1-l_royalty)*(l_nri+l_royalty*l_ori)
+        l_flow['net_ad_val_tax'] = l_flow['ad_val_tax']/(1-l_royalty)*(l_nri+l_royalty*l_ori)
+        l_flow['net_taxes'] = l_flow['taxes']/(1-l_royalty)*(l_nri+l_royalty*l_ori)
+        l_flow['net_capex'] = l_flow['wi_capex']
+        l_flow['net_cf'] = (
+            l_flow['net_revenue']
+            - l_flow['net_royalty']
+            - l_flow['net_expense']
+            - l_flow['net_taxes']
+            - l_flow['net_capex']
+        )
+        l_flow['net_dcf'] = (l_flow['net_cf'].to_numpy() / (1+self._discount_rate)**np.arange(0, len(l_flow['cf'].to_numpy())))
 
-        # calculate WI vales
+        l_flow['wi_net_oil'] = l_flow[self.OIL_COL]*(l_nri+l_royalty*l_ori)
+        l_flow['wi_net_gas'] = l_flow['gas_sold']*(l_nri+l_royalty*l_ori)
+        l_flow['wi_net_ngl'] = l_flow['ngl_volume']*(l_nri+l_royalty*l_ori)
 
         return l_flow
 