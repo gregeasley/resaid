56ad479dd4dbac54a25a4380734d16b03187d6b6
gregeasley <14328772+gregeasley@users.noreply.github.com>
2025-09-02 11:33:36 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="resaid",
-    version="0.2.1",
+    version="0.2.2",
     author="Greg Easley",
     author_email="greg@easley.dev",
     license="MIT",
